301d3e06a96de9132799ba4b164f501820b364d0
Peter L Jones <peter@drealm.info>
2020-01-19 12:37:59 +0000

    Add script to recover a recording directory with no RPP

---
+++ recover-recording.sh
@@ -0,0 +1,100 @@
+#!/bin/bash -e
+if [[ "$1" == "--help" ]]
+then
+cat <<-HELPMSG
+	Recreate a Reaper RPP project file from a Jamulus recording directory.
+
+	$(basename "$0") [--help]
+	  --help  display this help message
+
+	The intent of this script is to take an existing collection of Jamulus recorded
+	WAVE files and generate a Reaper RPP project file that matches the one the server
+	should have created.  It may sometimes be needed, for example when the server fails
+	to terminate the recording correctly (as that is when the RPP file gets written).
+
+	The script should be run from the directory containing the "failed" recording.
+	It outputs the RPP file on stdout - redirect this to your chosen project filename.
+
+	It requires  /etc/init.d/Jamulus  to be the provided start up script, so it can
+	read the configuration settings.
+
+HELPMSG
+	exit 0
+fi
+
+# Get the variables
+INIT_SCRIPT=$(realpath /etc/init.d/Jamulus)
+test -f "${INIT_SCRIPT}" || { echo 'Jamulus init script not found.'; exit 1; }
+. <(grep -v 'exit 1' "${INIT_SCRIPT}") >/dev/null 2>&1
+
+siteNamespace=$(uuidgen -n @url -N jamulus:${JAMULUS_SERVERNAME} --sha1)
+projectName="$(basename $(pwd))"
+
+if [[ "${projectName:0:4}" != "Jam-" ]]
+then
+	echo "Run this script from the failed recording directory." >&2
+	exit 1
+fi
+
+if [[ "$(echo *.wav)" == "*.wav" ]]
+then
+	echo "There are no WAVE files in this directory from which to create a project." >&2
+	exit 1
+fi
+
+projectNamespace=$(uuidgen -n $siteNamespace -N "${projectName}" --sha1)
+projectDate=$( p=${projectName#Jam-}; echo ${p:0:4}-${p:4:2}-${p:6:2} ${p:9:2}:${p:11:2}:${p:13:2}.${p:15} )
+secondsAt48K () {
+	echo $1 | awk '{ printf "%.14f\n", $1 * 128 / 48000; }'
+}
+
+echo '<REAPER_PROJECT 0.1 "5.0"' $(date -d "$projectDate" -u '+%s')
+echo ' RECORD_PATH "" ""'
+echo ' SAMPLERATE 48000 0 0'
+echo ' TEMPO 120 4 4'
+
+iid=0
+prevIP=''
+for x in $(ls -1 *.wav | sort -t- -k2)
+do
+	IP=${x#*-}
+	IP=${IP%%-*}
+	if [[ "$prevIP" != "$IP" ]]
+	then
+		iidt=0
+		if [[ "$prevIP" != "" ]]
+		then
+			echo '  NAME '$trackName
+			echo ' >'
+		fi
+		prevIP="$IP"
+		echo ' <TRACK {'$(uuidgen -n $projectNamespace -N $IP --sha1)'}'
+		echo '  TRACKID {'$(uuidgen -n $projectNamespace -N $IP --sha1)'}'
+		trackName="${x%-*-*.wav}"
+	else
+		[[ "${trackName:0:5}" == "____-" ]] && trackName="${x%-*-*.wav}"
+	fi
+	(( iid++ )) || true
+	(( iidt++ )) || true
+	echo '  <ITEM'
+	echo '   FADEIN 0 0 0 0 0 0'
+	echo '   FADEOUT 0 0 0 0 0 0'
+	filePos=${x#*-*-}
+	filePos=${filePos%-*.wav}
+	echo '   POSITION '$(secondsAt48K $filePos)
+	fileSize=$(stat -c '%s' "$x")
+	echo '   LENGTH '$(secondsAt48K $(( $fileSize / 64 )) )
+	echo '   IID '$iid
+	echo '   NAME '$IP' ('$iidt')'
+	echo '   GUID {'$(uuidgen -n $projectNamespace -N "$IP-$iidt" --sha1)'}'
+	echo '   <SOURCE WAVE'
+	echo '    FILE "'$x'"'
+	echo '   >'
+	echo '  >'
+done
+if [[ "$prevIP" != "" ]]
+then
+	echo '  NAME '$trackName
+	echo ' >'
+fi
+echo '>'
